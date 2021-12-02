3a2f484b45dfa8acca502a22bd1c56fab6c5b6bd
Adrian <y@thgie.ch>
2021-12-02 15:42:00 +0100

    fixed models for correct relations

--- ModelType.php
+++ ModelType.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class ModelType extends Model
 {
     protected $connection = 'pia';
-    protected $table = 'models';
     
     protected $fillable = [
         'label',
